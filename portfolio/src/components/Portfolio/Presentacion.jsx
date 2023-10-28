9753c59126c8ed5d605e3b20e52ec8b3d0edd897
Carolina Pallaro <charopallaro@gmail.com>
2023-10-28 04:11:15 -0300

    logo

--- portfolio/src/components/Portfolio/Presentacion.jsx
+++ portfolio/src/components/Portfolio/Presentacion.jsx
@@ -8,7 +8,7 @@ export const  Presentacion= () => {
          <div className={presentCSS.CircleImage}>
             <img 
             className={presentCSS.miImagen}
-            src="/public/imgs/miFoto.png" 
+            src="/imgs/miFoto.png" 
             alt="Imagen" 
             />
         </div>
